29a6c4d5ca8792e0e7b3db84a9a65ca0b3c15755
ayitteyg <ayitteyg@yahoo.com>
2025-11-26 10:15:05 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MySalesBook',
+  webDir: 'www'
+};
+
+export default config;
